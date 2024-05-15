ebe0cf75a6375f8521222247e5110729841af17f
murtazahmed05 <murtazahmed001@gmail.com>
2024-05-15 17:40:27 +0500

    go

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ToDoList React Application
+
+# 1st we make a component by using a shortcut [rafce]
+
+# 2nd step is to write JSX markup inside Return and we write it inside of fragments
+
+[ <> </> ]
+
+# 3rd step is
--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,22 @@ const page = () => {
 	const [title, settitle] = useState("");
 	const [mainTask, setmainTask] = useState([]);
 	const [error, seterror] = useState("");
+	const [editIndex, seteditIndex] = useState(null);
 
 	const submitHandler = (e) => {
 		e.preventDefault();
 		if (title.trim() !== "") {
-			setmainTask([...mainTask, { title }]);
-			settitle("");
-			seterror("");
+			if (editIndex !== null) {
+				let updatedTask = [...mainTask];
+				updatedTask[editIndex] = { title };
+				setmainTask(updatedTask);
+				settitle("");
+				seteditIndex(null);
+			} else {
+				setmainTask([...mainTask, { title }]);
+				settitle("");
+				seterror("");
+			}
 		} else {
 			seterror("Please Enter Something!");
 		}
@@ -24,6 +33,11 @@ const page = () => {
 		setmainTask(copyTask);
 	};
 
+	const editHandler = (i) => {
+		settitle(mainTask[i].title);
+		seteditIndex(i);
+	};
+
 	let renderTask = <h1 className="text-center">No Task Available</h1>;
 
 	if (mainTask.length > 0) {
@@ -31,21 +45,36 @@ const page = () => {
 			return (
 				<div key={i} className="d-flex justify-content-between mb-4">
 					<h2>{t.title}</h2>
-					<button
-						onClick={() => {
-							deleteHandler();
-						}}
-						className="btn btn-outline-danger"
-					>
-						X
-					</button>
+					<div className="d-flex align-items-center">
+						<button
+							onClick={() => {
+								editHandler(i);
+							}}
+							className="btn btn-outline-success me-3"
+						>
+							Edit
+						</button>
+						<button
+							onClick={() => {
+								deleteHandler(i);
+							}}
+							className="btn btn-outline-danger"
+						>
+							X
+						</button>
+					</div>
 				</div>
 			);
 		});
 	}
 	return (
 		<>
+			{/* application title in h1 Tag */}
+
 			<h1 className="p-4 text-bg-primary text-center">ToDoList</h1>
+
+			{/* Form below Contaning an Input field */}
+
 			<form className="d-flex align-items-center justify-content-center" onSubmit={submitHandler}>
 				<input
 					className="px-4 py-2 my-5 border rounded-1"
@@ -57,10 +86,16 @@ const page = () => {
 				/>
 				<button className="btn btn-outline-primary ">Add</button>
 			</form>
+
 			{error && <p className="text-danger text-center">{error}</p>}
+
+			{/* This div will show user Tasks */}
+
 			<div className="p-4 bg-primary-subtle">
 				<ul>{renderTask}</ul>
 			</div>
+
+			<p className="text-white text-center mt-5">React.js Project!</p>
 		</>
 	);
 };
